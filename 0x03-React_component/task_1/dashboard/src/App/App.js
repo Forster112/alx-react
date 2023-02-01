818e9f3979bad5d880a3992bcfdeca900e539622
Forster112 <forstermichael112@gmail.com>
2023-02-01 23:17:51 +0100

    component

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import React from 'react'
+import Notifications from '../Notifications/Notifications'
+import Header from '../Header/Header'
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer'
+import { getLatestNotification } from '../utils/utils';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types'
+
+
+class App extends React.Component {
+
+  static listCourses = [
+    {id: 1, name: 'ES6', credit: 60},
+    {id: 2, name: 'Webpack', credit: 20},
+    {id: 3, name: 'React', credit: 40},
+  ];
+
+  static listNotifications = [
+    {id: 1, value: "New course available", type: "default"},
+    {id: 2, value: "New resume available", type: "urgent"},
+    {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
+  ];
+
+  constructor(props){
+    super(props);
+    this.isLoggedIn = props.isLoggedIn;
+    this.logOut = props.logOut;
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleKeyDown(e){
+    e.preventDefault()
+    if(e.ctrlKey && e.key === 'h'){
+      alert("Logging you out")
+      this.logOut();
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        < Notifications listNotifications={this.listNotifications} />
+        <div className="App">
+          < Header />
+          <div className="App-body">
+            { this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login /> }
+          </div>
+          <div className="App-footer">
+            < Footer />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+}
+
+export default App;
