de288a29a539c3a7f23ff8187f2c26e7ffbaf4f9
Forster112 <forstermichael112@gmail.com>
2023-02-21 00:33:23 +0100

    immutable javascript

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { fromJS } from 'immutable';
+
+const accessImmutableObject = (object, array) => {
+  return fromJS(object).getIn(array, undefined);
+};
+
+export default accessImmutableObject;
